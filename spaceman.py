59d6d1f7f33f28526be4f103679219df4e28bbb0
Andrey Novichkov <andrey.novichkov@gmail.com>
2019-09-08 20:37:10 -0700

    [addition] Added color to terminal output, option to quit game

--- spaceman.py
+++ spaceman.py
@@ -1,5 +1,12 @@
 from random import choice
-import re
+
+
+def print_green(input_string):
+    print("\033[92m{}\033[00m" .format(input_string))
+
+
+def print_red(input_string):
+    print("\033[91m{}\033[00m" .format(input_string))
 
 
 def load_word():
@@ -38,22 +45,27 @@ def is_guess_in_word(guess, secret_word):
 
 
 def print_welcome_message():
+    print("-------------------------------------------------------------------------------------------------")
     print("Welcome! This is a game of Spaceman")
     print("I have thought of a word, and it is your job to guess it letter by letter in no particular order")
     print("You can guess the wrong letter at most 7 times, if you make 8 mistakes or more, I win! ")
     print("If you are able to guess the word within 7 strikes, then you win! Happy playing! ")
-    print("--------------------------------------------------------------------------------")
+    print("--------------------------------------------------------------------------------------------------")
 
 
-def get_input_letter():
+def get_input_letter(letters_guessed):
     letter_input = ""
 
     while True:
         letter_input = input("Please enter a letter: ")
         if validate_input(letter_input):
-            break
+            # Checks if letter has been guessed already
+            if letter_input in letters_guessed:
+                print_red("You already entered " + letter_input + ". Try again!")
+            else:
+                break
         else:
-            print("You entered an invalid letter, try again!")
+            print_red("You entered an invalid letter, try again!")
 
     return letter_input.lower()
 
@@ -63,32 +75,45 @@ def validate_input(input):
 
 
 def main():
-    chosen_word = load_word()
-    chosen_word_in_chars = list(chosen_word)
-    length_of_chosen_word = len(chosen_word_in_chars)
-
-    letters_guessed = []
-
-    strikes_remaining = 7
 
     print_welcome_message()
 
-    while strikes_remaining > 0 and not is_word_guessed(chosen_word_in_chars, letters_guessed):
-        print("The length of the word is: " + str(length_of_chosen_word) + " and you have " + str(strikes_remaining) + " strikes remaining")
-        input_letter = get_input_letter()
-        letters_guessed.append(input_letter)
+    while True:
+        # Some variables
+        chosen_word = load_word()
+        chosen_word_in_chars = list(chosen_word)
+        length_of_chosen_word = len(chosen_word_in_chars)
 
-        if not is_guess_in_word(input_letter, chosen_word_in_chars):
-            strikes_remaining -= 1
-            print("Incorrect guess")
+        letters_guessed = []
 
-        print(get_guessed_word(chosen_word_in_chars, letters_guessed))
+        strikes_remaining = 7
 
-    if strikes_remaining == 0:
-        print("I am very sorry, but you have lost :(")
-        print("The word was: " + chosen_word)
-    else:
-        print(chosen_word + " is the word!! You have won!!!")
+        # Game control
+        while strikes_remaining > 0 and not is_word_guessed(chosen_word_in_chars, letters_guessed):
+            print("The length of the word is: " + str(length_of_chosen_word) + " and you have " + str(strikes_remaining) + " strikes remaining")
+            input_letter = get_input_letter(letters_guessed)
+
+            letters_guessed.append(input_letter)
+
+            if not is_guess_in_word(input_letter, chosen_word_in_chars):
+                strikes_remaining -= 1
+                print_red("Incorrect guess")
+
+            print_green(get_guessed_word(chosen_word_in_chars, letters_guessed))
+
+        if strikes_remaining == 0:
+            print("I am very sorry, but you have lost :(")
+            print_green("The word was: " + chosen_word)
+        else:
+            print(chosen_word + " is the word!! You have won!!!")
+
+        # Ask user to play again
+        if input("Please enter Y if you want to play again, any other character to quit: ").lower() == "y":
+            print("------------------------------------------------")
+            print("Ok! Get ready to play again! ")
+            print("------------------------------------------------")
+        else:
+            print("Sad to see you go, hope to see you soon again!!")
 
 
 if __name__ == "__main__":
